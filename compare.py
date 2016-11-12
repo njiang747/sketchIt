2170de142587b856de82ccdb7fac659b6653df0f
JonZha <jonzha39@gmail.com>
2016-11-11 21:40:24 -0500

    awef
    awef

---
+++ compare.py
@@ -0,0 +1,6 @@
+# Takes in a given query edgel set and the edgel_table and returns the top 5000 image matches
+def query_compare (query_set, edgel_table):
+    for 
+
+# Takes in 5000 images and returns the top n matches when compared to query
+def database_compare(images, query, n):
