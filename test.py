a82d115a779dde04bb60b5f1687a6274a67a61c9
Nicholas Jiang <Nicholas Jiang>
2016-11-13 04:50:11 -0500

    updated test GUI

--- test.py
+++ test.py
@@ -18,55 +18,63 @@ def top_hits(filename):
         plt.subplot(2,3,i+2), plt.imshow(cv2.cvtColor(cv2.imread(match,1), cv2.COLOR_BGR2RGB)), plt.axis('off')
     plt.show()
 
+def top_hits_tag(filename, tag):
+    raw_img = cv2.imread(filename, 0)
+    top = searcher.top_n_mult(raw_img, 50, tag=tag)
+    for match in top:
+        print match
+    plt.subplot(2, 3, 1), plt.imshow(raw_img, cmap='gray'), plt.axis('off')
+    for i,match in enumerate(top[:5]):
+        plt.subplot(2,3,i+2), plt.imshow(cv2.cvtColor(cv2.imread(match,1), cv2.COLOR_BGR2RGB)), plt.axis('off')
+    plt.show()
+
 if os.path.isfile('save_state.csv'):
     searcher = search.Search('images', 'save_state.csv')
 else:
     searcher = search.Search('images')
     save_hits()
 
-while True:
-    screen = pygame.display.set_mode((800, 800))
 
-    draw_on = False
-    last_pos = (0, 0)
-    color = (255, 255, 255)
-    radius = 2
+screen = pygame.display.set_mode((800, 800))
 
-    def roundline(srf, color, start, end, radius=1):
-        dx = end[0] - start[0]
-        dy = end[1] - start[1]
-        distance = max(abs(dx), abs(dy))
-        for i in range(distance):
-            x = int(start[0] + float(i) / distance * dx)
-            y = int(start[1] + float(i) / distance * dy)
-            pygame.draw.circle(srf, color, (x, y), radius)
+draw_on = False
+last_pos = (0, 0)
+color = (255, 255, 255)
+radius = 2
 
-    try:
-        while True:
-            e = pygame.event.wait()
-            if e.type == pygame.QUIT:
-                raise StopIteration
-            if e.type == pygame.MOUSEBUTTONDOWN:
-                # color = (random.randrange(256), random.randrange(256), random.randrange(256))
+def roundline(srf, color, start, end, radius=1):
+    dx = end[0] - start[0]
+    dy = end[1] - start[1]
+    distance = max(abs(dx), abs(dy))
+    for i in range(distance):
+        x = int(start[0] + float(i) / distance * dx)
+        y = int(start[1] + float(i) / distance * dy)
+        pygame.draw.circle(srf, color, (x, y), radius)
+
+try:
+    while True:
+        e = pygame.event.wait()
+        if e.type == pygame.QUIT:
+            raise StopIteration
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            pygame.draw.circle(screen, color, e.pos, radius)
+            draw_on = True
+        if e.type == pygame.MOUSEBUTTONUP:
+            draw_on = False
+        if e.type == pygame.MOUSEMOTION:
+            if draw_on:
                 pygame.draw.circle(screen, color, e.pos, radius)
-                draw_on = True
-            if e.type == pygame.MOUSEBUTTONUP:
-                draw_on = False
-            if e.type == pygame.MOUSEMOTION:
-                if draw_on:
-                    pygame.draw.circle(screen, color, e.pos, radius)
-                    roundline(screen, color, e.pos, last_pos, radius)
-                last_pos = e.pos
-            pygame.display.flip()
+                roundline(screen, color, e.pos, last_pos, radius)
+            last_pos = e.pos
+        if e.type == pygame.KEYDOWN:
+            pygame.image.save(screen, 'temp.jpg')
+            screen.fill(color)
+            top_hits('temp.jpg')
+            screen = pygame.display.set_mode((800, 800))
+        pygame.display.flip()
 
 
-    except StopIteration:
-        pass
-    pygame.image.save(screen, 'temp.jpg')
-    _img = open('temp.jpg', 'rb')
-    _out = _img.read()
-    _img.close()
-    pygame.quit()
-
-    top_hits('temp.jpg')
-    cv2.waitKey(0)
+except StopIteration:
+    pass
+pygame.image.save(screen, 'temp.jpg')
+pygame.quit()
