49ae480ce8454f60ecbe84fc6937c041a63902c2
yeduvaka <yeduvaka@princeton.edu>
2016-11-12 20:31:18 -0500

    database.py can handle edge counts now

--- database.py
+++ database.py
@@ -8,15 +8,17 @@ def load_data(hitmap, filename):
         for col in range(0, len(hitmap[row])):
             for deg in range(0, len(hitmap[row][col])):
                 hitmap[row][col][deg] = set(raw_data[row * len(hitmap) + col * len(hitmap[row]) + deg])
-    return hitmap
+    edgel_count = raw_data[-1]
+    return hitmap, edgel_count
 
 
-def save_data(hitmap, filename):
+def save_data(hitmap,edgel_count, filename):
     f = open(filename, 'wb')
     writer = csv.writer(f)
     for row in hitmap:
         for col in row:
             for deg in col:
                 writer.writerow(list(deg))
+    writer.writerow(edgel_count)
     f.close()
 
