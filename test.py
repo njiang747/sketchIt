768a0bb088ec878ace1e03df202d183295e31e61
Nicholas Jiang <Nicholas Jiang>
2016-11-13 06:15:18 -0500

    big improvements to sketch UI and added tag searching interface

--- test.py
+++ test.py
@@ -8,19 +8,9 @@ import os
 def save_hits():
     database.save_data(searcher.pre.get_hitmap(), searcher.pre.get_edgel_counts(), 'save_state.csv')
 
-def top_hits(filename):
+def top_hits(filename, tag=None):
     raw_img = cv2.imread(filename, 0)
-    top = searcher.top_n_mult(raw_img, 50)
-    for match in top:
-        print match
-    plt.subplot(2, 3, 1), plt.imshow(raw_img, cmap='gray'), plt.axis('off')
-    for i,match in enumerate(top[:5]):
-        plt.subplot(2,3,i+2), plt.imshow(cv2.cvtColor(cv2.imread(match,1), cv2.COLOR_BGR2RGB)), plt.axis('off')
-    plt.show()
-
-def top_hits_tag(filename, tag):
-    raw_img = cv2.imread(filename, 0)
-    top = searcher.top_n_mult(raw_img, 50, tag=tag)
+    top = searcher.top_n_mult(raw_img, 50, tag)
     for match in top:
         print match
     plt.subplot(2, 3, 1), plt.imshow(raw_img, cmap='gray'), plt.axis('off')
@@ -35,12 +25,21 @@ else:
     save_hits()
 
 
+pygame.init()
 screen = pygame.display.set_mode((800, 800))
 
 draw_on = False
 last_pos = (0, 0)
 color = (255, 255, 255)
+black = (0, 0, 0)
 radius = 2
+sketch = pygame.Surface(screen.get_size())
+sketch = sketch.convert()
+sketch.fill(black)
+tag_text = ''
+font = pygame.font.SysFont('segoeui', 48)
+text = font.render(tag_text, 1, (0,255,255))
+textpos = text.get_rect()
 
 def roundline(srf, color, start, end, radius=1):
     dx = end[0] - start[0]
@@ -57,20 +56,48 @@ try:
         if e.type == pygame.QUIT:
             raise StopIteration
         if e.type == pygame.MOUSEBUTTONDOWN:
-            pygame.draw.circle(screen, color, e.pos, radius)
+            pygame.draw.circle(sketch, color, e.pos, radius)
             draw_on = True
         if e.type == pygame.MOUSEBUTTONUP:
             draw_on = False
         if e.type == pygame.MOUSEMOTION:
             if draw_on:
-                pygame.draw.circle(screen, color, e.pos, radius)
-                roundline(screen, color, e.pos, last_pos, radius)
+                pygame.draw.circle(sketch, color, e.pos, radius)
+                roundline(sketch, color, e.pos, last_pos, radius)
             last_pos = e.pos
         if e.type == pygame.KEYDOWN:
-            pygame.image.save(screen, 'temp.jpg')
-            screen.fill(color)
-            top_hits('temp.jpg')
-            screen = pygame.display.set_mode((800, 800))
+            if e.key == 13:
+                pygame.image.save(sketch, 'temp.jpg')
+                text2 = font.render("Searching for Matches...", 1, (128,128,128))
+                textpos2 = text2.get_rect()
+                textpos2.centerx = screen.get_rect().centerx
+                textpos2.centery = 50
+                screen.blit(sketch, (0,0))
+                screen.blit(text2, textpos2)
+                pygame.display.flip()
+                if len(tag_text) > 0:
+                    top_hits('temp.jpg', tag_text)
+                else:
+                    top_hits('temp.jpg')
+                tag_text = ''
+                text = font.render(tag_text, 1, (128,128,128))
+                screen.fill(black)
+                sketch.fill(black)
+            elif e.key in range(256) and (chr(e.key).isalpha() or e.key == 32):
+                tag_text += chr(e.key)
+                text = font.render(tag_text, 1, (128,128,128))
+                textpos = text.get_rect()
+                textpos.centerx = screen.get_rect().centerx
+                textpos.centery = 50
+            elif e.key == 8:
+                tag_text = tag_text[:-1]
+                text = font.render(tag_text, 1, (128,128,128))
+                textpos = text.get_rect()
+                textpos.centerx = screen.get_rect().centerx
+                textpos.centery = 50
+
+        screen.blit(sketch, (0,0))
+        screen.blit(text, textpos)
         pygame.display.flip()
 
 
